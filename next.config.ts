aa219b24c3c68fcc2d78296a33aa410500b25f33
Deepak Sharma <dasjideepak@gmail.com>
2026-02-28 11:14:10 +0530

    chore: update ESLint configuration, integrate Next.js Module Federation, and add DashboardApp component

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,27 @@
 import type { NextConfig } from "next";
+import NextFederationPlugin from "@module-federation/nextjs-mf";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
   reactStrictMode: true,
+
+  webpack(config) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "remote2",
+        filename: "static/chunks/remoteEntry.js",
+        exposes: {
+          "./DashboardApp": "./src/components/DashboardApp",
+        },
+        shared: {
+          react: { singleton: true, requiredVersion: "^18.0.0" },
+          "react-dom": { singleton: true, requiredVersion: "^18.0.0" },
+        },
+        extraOptions: {},
+      })
+
+    );
+    return config;
+  },
 };
 
 export default nextConfig;
---
+++ src/components/DashboardApp.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function DashboardApp() {
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <h1>Remote2 App</h1>
+      </main>
+    </div>
+  );
+}
