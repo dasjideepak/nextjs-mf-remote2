160bf439469d97e9967ed0bbe5e09b80196ee9a0
Deepak Sharma <dasjideepak@gmail.com>
2026-02-28 11:37:57 +0530

    chore: add Prettier configuration, update ESLint setup, and modify package scripts for formatting and linting

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        {/* eslint-disable-next-line @next/next/no-title-in-document-head */}
+        <title>Remote2</title>
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
